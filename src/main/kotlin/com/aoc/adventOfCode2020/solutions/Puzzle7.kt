c87c8b6669c03fe5c38b00cc9ef5d111d740e8fc
Stephen Roberts <stephen.roberts.skybettingandgaming.com>
2020-12-05 22:19:01 +0000

    Dirty solution to Problem 7

--- src/main/kotlin/com/aoc/adventOfCode2020/solutions/Puzzle7.kt
+++ src/main/kotlin/com/aoc/adventOfCode2020/solutions/Puzzle7.kt
@@ -2,6 +2,7 @@ package com.aoc.adventOfCode2020.solutions
 
 import org.springframework.stereotype.Component
 import java.io.File
+import java.lang.Exception
 
 @Component
 class Puzzle7 {
@@ -9,79 +10,62 @@ class Puzzle7 {
         val prefix = "/Users/sro01/Documents/002_PersonalCode/adventOfCode2020/src/main/kotlin/com/aoc/adventOfCode2020/inputs/"
         val inputPuzzle = "inputDay4.txt"
         val rawFile = readFileLineByLineUsingForEachLine("${prefix}${inputPuzzle}")
-        println(rawFile)
 
+        val possPassportList = createPossiblePassportList(rawFile)
+        val validPassports = filterForValidPassports(possPassportList)
+        println("HERES YOUR VALID PASSPORTS....")
+        println(validPassports)
+        println(validPassports.size)
+    }
+
+
+    fun createPossiblePassportList(file: List<String>): MutableList<Map<String, Any>> {
         var list = mutableListOf<Map<String, Any>>()
-        val tempMap = mutableMapOf<String, Any>()
+        var tempMap = mutableMapOf<String, Any>()
 
-    for((index, value) in rawFile.withIndex()){
-        if(value.isBlank() || index == rawFile.size-1){
-            println("HELLO")
-            list.add(tempMap)
-            println("LIST $list")
-            tempMap.clear()
-        } else {
-            val split = rawFile[index].split(":")
-            println("SPLIT $split")
-            tempMap[split[0]] = split[1]
-            println("TEMP $tempMap")
+        for ((index, value) in file.withIndex()) {
+            if (value.isBlank() || index == file.size - 1) {
+                list.add(tempMap)
+                tempMap = mutableMapOf<String, Any>()
+            } else {
+                val splitBySpace = file[index].split(" ")
+                val splitByColon = splitBySpace.map { it.split(":")}
+
+                for(entry in splitByColon) {
+                    tempMap[entry[0]] = entry[1]
+                }
+            }
         }
+        return list
     }
-println("LIST $list")
-    }
-//
-//    fun addToList(map: Map<String, Any>, list: MutableList<Map<String, Any>>): MutableList<Map<String, Any>> {
-////        list.add(map)
-//        list.add(list.size,map)
-//        return list
-//    }
 
-    fun readFileLineByLineUsingForEachLine(filename: String): List<String>{
+    fun filterForValidPassports(possPassportList: MutableList<Map<String, Any>>): List<Passport>{
+        val passportList = mutableListOf<Passport>()
+        for(passport in possPassportList) {
+            try {
+                val byr = passport["byr"] as String
+                val iyr = passport["iyr"] as String
+                val eyr = passport["eyr"] as String
+                val hgt = passport["hgt"] as String
+                val hcl = passport["hcl"] as String
+                val ecl = passport["ecl"] as String
+                val pid = passport["pid"] as String
+                passportList.add(Passport(byr, iyr, eyr, hgt, hcl, ecl, pid))
+                println(Passport(byr, iyr, eyr, hgt, hcl, ecl, pid))
+            } catch (e: Exception) {
+                println(e)
+            }
+        }
+        return passportList
+    }
+
+    fun readFileLineByLineUsingForEachLine(filename: String): List<String> {
         val list = mutableListOf<String>()
         File(filename).forEachLine {
             list.add(it)
         }
         return list
     }
-
-    fun checkEndOfPassportEntry(entry: String): Boolean {
-        return entry.isBlank()
-    }
-
-//    fun readFileLineByLineUsingForEachLine(fileName: String) {
-//        var list = mutableListOf<Map<String, Any>>()
-//        var tempMap= mutableMapOf<String, Any>()
-////        var list = emptyList<Map<String, Any>>()
-//
-//
-//
-//        File(fileName).forEachLine { it ->
-//            if (it.isBlank()) {
-//                println("BLANK LINE")
-//                list.add(tempMap)
-//                println("NEW LIST $list")
-//                tempMap.clear()
-//                println("EMPTYING MAP")
-//                println(tempMap)
-//            } else {
-//                println("GOT AN ENTRY")
-//                println(it)
-//                var splitEntry = it.split(" ")
-//                println("SPLIT $splitEntry")
-//                for (entry in splitEntry){
-//                    println("Entry $entry")
-//                    val splitByColon = entry.split(":")
-//                    println("COLON $splitByColon")
-//                    tempMap[splitByColon[0]] = splitByColon[1]
-//                }
-//                println("NEW MAP $tempMap")
-//                println("WAITING LIST $list")
-//            }
-////            list.add(tempMap)
-//        }
-//
-////        return list
-//    }
 }
 
-data class Passport(val byr: Int, val iyr: Int, val eyr: Int, val hgt: String, val hcl: String, val ecl: String, val pid: Int, val cid: Int?)
+data class Passport(val byr: String, val iyr: String, val eyr: String, val hgt: String, val hcl: String, val ecl: String, val pid: String, val cid: String? = null)
